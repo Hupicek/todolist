3e7ce0f2986265c1b50104266be17b53ec34f653
jan.ptacek <jan.ptacek@VT2-15>
2024-01-25 10:42:58 +0100

    Přidejte soubory projektu.

---
+++ todolist/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace ToDoList
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tasks = new ObservableCollection<TaskItem>();
+            taskListBox.ItemsSource = Tasks;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string taskName = taskTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(taskName))
+            {
+                Tasks.Add(new TaskItem { TaskName = taskName });
+                taskTextBox.Clear();
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (taskListBox.SelectedItem != null)
+            {
+                Tasks.Remove((TaskItem)taskListBox.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to remove.");
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Tasks.Clear();
+        }
+    }
+
+    public class TaskItem : INotifyPropertyChanged
+    {
+        private string _taskName;
+        private bool _isCompleted;
+
+        public string TaskName
+        {
+            get { return _taskName; }
+            set
+            {
+                _taskName = value;
+                OnPropertyChanged("TaskName");
+            }
+        }
+
+        public bool IsCompleted
+        {
+            get { return _isCompleted; }
+            set
+            {
+                _isCompleted = value;
+                OnPropertyChanged("IsCompleted");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
